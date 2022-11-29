a37aed54d42e16117beae6b01df8d0717b9f123d
lazuadik <90018535+lazuadik@users.noreply.github.com>
2022-11-29 22:10:51 +0700

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db=pg_connect('host=localhost dbname=ittomorrow user=postgres password=22September_ port=5432');
-if( !$db ){
-    die("Gagal terhubung dengan database: " . pg_connect_error());
-}
-?>
